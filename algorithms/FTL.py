d37b0946b12dbb243bf0f43ac038e56f8e749b72
Pedram Haqiqi <63091219+pedramhaqiqi@users.noreply.github.com>
2023-10-15 13:12:07 -0400

    Regret Maximizer using bernouli(1/2)

--- algorithms/FTL.py
+++ algorithms/FTL.py
@@ -2,8 +2,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 #[0,1]
-BEST_CHOICE = 0.5 
-ITERATIONS = 2000
+BEST_CHOICE = 0.5
+ITERATIONS = 1000
 
 def calculate_regret(predicted, actual):
     """Calculate the regret for given choices."""
@@ -17,49 +17,64 @@ def total_loss(predicted, actual):
 user_decisions = np.array([])
 enemy_decisions = np.array([])
 stored_regret = np.array([])
-ftl_loss_data = np.array([])
+our_cum_loss = np.array([])
 best_case_cum_loss = np.array([])
 
 for step in range(ITERATIONS):
     user_value = enemy_decisions.mean()
     if not user_decisions.size:
         user_value = BEST_CHOICE   
-    enemy_value = np.random.uniform()
+    # Maximize FTL over [0,1], try with uniform and see that the regret is less. 
+    # This is showing that we can have a tigher lower bound on the regret.  sigma^2 Ω (log T) < Regret < O(logT)
+    # So as yourself, what distribution on [0,1] at each sample, will have biggest variance? 
+    enemy_value = np.random.binomial(1, 1/2) 
     
     enemy_decisions = np.hstack([enemy_decisions, enemy_value])
     user_decisions = np.hstack([user_decisions, user_value])
     
     stored_regret = np.hstack([stored_regret, calculate_regret(user_decisions, enemy_decisions)])
-    ftl_loss_data = np.hstack([ftl_loss_data, total_loss(user_decisions, enemy_decisions)])
+    our_cum_loss = np.hstack([our_cum_loss, total_loss(user_decisions, enemy_decisions)])
     best_case_cum_loss = np.hstack([best_case_cum_loss, total_loss(np.full_like(enemy_decisions, BEST_CHOICE), enemy_decisions)])
 
+# Create a figure with three vertically stacked subplots
+plt.figure(figsize=(10, 15))
 
-# Plotting the stored regret over time
-plt.figure(figsize=(10, 5))
+# First subplot: Stored Regret
+plt.subplot(3, 1, 1)  # 3 rows, 1 column, first subplot
 plt.plot(stored_regret, label="Stored Regret")
 plt.title("Regret over Time")
 plt.xlabel("Time")
 plt.ylabel("Regret")
 plt.legend()
 plt.grid(True)
-plt.show()
 
-# Plotting the follow-the-leader loss over time
-plt.figure(figsize=(10, 5))
-plt.plot(ftl_loss_data, label="FTL Loss")
-plt.title("FTL Loss over Time")
+# Add a log function on top of the Regret plot
+x = np.linspace(1, len(stored_regret), len(stored_regret))
+log_curve = 1/10 * np.log(x)
+plt.plot(x, log_curve, label="log(x)", linestyle="--", color="red")
+plt.legend()
+
+
+# Second subplot: Our cum Loss
+plt.subplot(3, 1, 2)  # 3 rows, 1 column, second subplot
+plt.plot(our_cum_loss, label="Our cum Loss")
+plt.title("Our cum loss over Time")
 plt.xlabel("Time")
 plt.ylabel("Loss")
 plt.legend()
 plt.grid(True)
-plt.show()
 
-# Plotting the average loss over time
-plt.figure(figsize=(10, 5))
-plt.plot(best_case_cum_loss, label="best single value loss")
-plt.title("best single over Time")
+# Third subplot: best single value loss
+plt.subplot(3, 1, 3)  # 3 rows, 1 column, third subplot
+plt.plot(best_case_cum_loss, label="Best single value loss")
+plt.title("Best Single Value Loss over Time")
 plt.xlabel("Time")
 plt.ylabel("Loss")
 plt.legend()
 plt.grid(True)
+
+# Adjust the layout
+plt.tight_layout()
+
+# Show the plots
 plt.show()
